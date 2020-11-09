e328eaa4388d02f2dcc6581883565fb2f759af00
Chris Sison <chris.a.sison@outlook.com>
2020-11-08 17:20:17 -0800

    async functions

--- netflix-react/src/db/mongoose.js
+++ netflix-react/src/db/mongoose.js
@@ -1,21 +1,6 @@
-'/Users/chris/mongodb/bin/mongod --dbpath=/Users/chris/mongodb-data'
-
 const mongoose = require('mongoose');
-const User = require('../models/user');
 
 mongoose.connect('mongodb://127.0.0.1:27017/netflix-react-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
-
-const account = new User({
-    name: "chris",
-    email: "c.sison@wsu.edu",
-    password: "Ardvark12!@"
-})
-
-account.save().then(() => {
-
-}).catch((error) => {
-    console.log('error ', error);
-}) 
--- netflix-react/src/server/index.js
+++ netflix-react/src/server/index.js
@@ -1,21 +1,47 @@
+'/Users/chris/mongodb/bin/mongod --dbpath=/Users/chris/mongodb-data'
+
 const express = require('express');
 require('../db/mongoose');
 const User = require('../models/user');
 
 const app = express();
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 
-app.post('/users', (req, res) => {
+
+app.post('/users', async (req, res) => {
     const user = new User(req.body);
 
+    try {
+        await user.save();
+        res.send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+})
 
-    user.save().then(() => {
-        res.send(user)
-    }).catch(() => {
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (error) {
+        res.status(404).send(error);
+    }
+})
 
-    })
+app.get('/users/:id', async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const user = await User.findById(_id);
+        if (!user) {
+            res.status(404).send();
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(500).send(error);
+    }
 })
 
 
